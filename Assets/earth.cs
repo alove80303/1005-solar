e3c04384649c5646d630d16ceb4b39a3dfd3e0c8
Pei-Xuan Wu <105502003@nccu.edu.tw>
2018-10-09 21:50:51 +0800

    First time commit

---
+++ Assets/earth.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class earth : MonoBehaviour {
+
+	public float y;
+
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		Transform transform = GetComponent<Transform>();
+		Vector3 angle = new Vector3(0, y + Time.deltaTime, 0);
+		transform.Rotate(angle);
+	}
+}
+
---
+++ Assets/sun.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sun : MonoBehaviour {
+
+	public float y;
+
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		Transform transform = GetComponent<Transform>();
+		Vector3 angle = new Vector3(0, y + Time.deltaTime, 0);
+		transform.Rotate(angle);
+	}
+}
